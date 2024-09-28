d4871fe91d9968578f1a7459ba362a0ca1ea156c
Mozinor95320 <43842362+Mozinor95320@users.noreply.github.com>
2024-09-28 17:46:41 +0000

    g

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
 
 <!-- Inclusion de Bootstrap JS et de son bundle -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="script_fiche_traca.js"></script>
+<script src="github/script_fiche_traca.js"></script>
 
 </body>
 </html>
